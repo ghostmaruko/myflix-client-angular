0941b77a81dc7d3c4ea569f386ae26e4324fe088
Marco Esu <102921659+ghostmaruko@users.noreply.github.com>
2025-12-18 12:07:18 +0100

    added authguard for /movies and /profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,18 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { UserProfileViewComponent } from './user-profile-view/user-profile-view.component';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
-  { path: 'movies', component: MovieCardComponent },
-  { path: 'profile', component: UserProfileComponent },
+  { path: 'movies', component: MovieCardComponent, canActivate: [AuthGuard] },
+  {
+    path: 'profile',
+    component: UserProfileComponent,
+    canActivate: [AuthGuard],
+  },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '**', redirectTo: 'welcome' },
   {
     path: 'profile',
     component: UserProfileViewComponent,
