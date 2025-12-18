dd6cc8cef84316d3670d2cd73e708175f709e3d2
Marco Esu <102921659+ghostmaruko@users.noreply.github.com>
2025-12-18 17:36:45 +0100

    Fix routing and Vercel config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,24 @@
 import { Routes } from '@angular/router';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { UserProfileViewComponent } from './user-profile-view/user-profile-view.component';
 import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+
   { path: 'welcome', component: WelcomePageComponent },
-  { path: 'movies', component: MovieCardComponent, canActivate: [AuthGuard] },
+
   {
-    path: 'profile',
-    component: UserProfileComponent,
+    path: 'movies',
+    component: MovieCardComponent,
     canActivate: [AuthGuard],
   },
-  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: '**', redirectTo: 'welcome' },
   {
     path: 'profile',
     component: UserProfileViewComponent,
+    canActivate: [AuthGuard],
   },
+
+  { path: '**', redirectTo: 'welcome' },
 ];
